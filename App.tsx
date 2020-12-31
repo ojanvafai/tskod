8c29f9ce79c0e5005d1366674631dcd58003536f
Tim Dresser <tdresser@gmail.com>
2020-12-31 14:46:10 -0500

    Shadow

--- App.tsx
+++ App.tsx
@@ -37,41 +37,12 @@ import {
 declare const global: { HermesInternal: null | {} };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
   engine: {
     position: 'absolute',
     right: 0,
   },
   body: {
     backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
@@ -149,7 +120,23 @@ interface CardProps {
 
 class Card extends React.Component<CardProps> {
   render() {
-    return <View><Text>{this.props.snippet}</Text></View>;
+    let cardStyle = { 
+      margin: 15, 
+      padding: 4, 
+      /*borderWidth: 1, */
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+      shadowOpacity: 0.32,
+      shadowRadius: 5.46,
+      backgroundColor: Colors.white,
+      elevation: 9,
+    };
+    return <View style={cardStyle}>
+      <Text>{this.props.snippet}</Text>
+    </View>;
   }
 }
 
