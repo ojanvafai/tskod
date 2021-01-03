24edb9efc87b02744beef13868dc349601339707
Tim Dresser <tdresser@gmail.com>
2021-01-02 21:21:01 -0500

    Dragging card does nothing.

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Animated, PanResponder } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 interface CardProps {
     snippet: string,
 }
 
+// Animated example here: https://snack.expo.io/?&iframeId=u1guni3npf&preview=true&platform=web&supportedPlatforms=ios,android,web&name=Animated&description=Example%20usage&theme=light&waitForData=true.
+
 export class Card extends React.Component<CardProps> {
+    pan = new Animated.ValueXY();
+    panResponder = PanResponder.create({
+        onMoveShouldSetPanResponder: () => {
+            console.log("Starting pan.")
+            return true;
+        },
+        onPanResponderMove: Animated.event([
+            null,
+            { dx: this.pan.x, dy: this.pan.y }
+        ], {
+            useNativeDriver: false // https://stackoverflow.com/questions/45363416/usenativedriver-with-panresponder
+        }),
+        onPanResponderRelease: () => {
+            Animated.spring(this.pan, {
+                toValue: { x: 0, y: 0 },
+                useNativeDriver: true
+            }).start();
+        }
+    });
     render() {
+        console.log("RENDER");
         let cardStyle = {
+            width: 100,
+            height: 100,
             margin: 15,
             padding: 4,
             /*borderWidth: 1, */
@@ -21,9 +45,11 @@ export class Card extends React.Component<CardProps> {
             shadowRadius: 5.46,
             backgroundColor: Colors.white,
             elevation: 9,
+            transform: [{ translateX: this.pan.x }, { translateY: this.pan.y }]
         };
-        return <View style={cardStyle}>
+
+        return <Animated.View style={cardStyle} {...this.panResponder.panHandlers}>
             <Text>{this.props.snippet}</Text>
-        </View>;
+        </Animated.View>;
     }
 }
