1b15029e45cf0a77f2b8a4f8f914de2637f26f51
ojan <1607171+ojanvafai@users.noreply.github.com>
2021-01-16 12:07:51 -0800

    Tidy up message headers
    - Only show the name instead of full from email addresses
    - Tweak font size / color / offsets
    - No wrap
    - Don't show seconds for times

--- src/ui/MessageComponent.tsx
+++ src/ui/MessageComponent.tsx
@@ -1,39 +1,61 @@
 import {Message} from '../Message';
 
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 const htmlHeader =
   '<meta name="viewport" content="width=device-width, initial-scale=1"><style>body, html { margin: 0 }</style>';
 
+const style = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    marginTop: 8,
+    marginBottom: 12,
+  },
+  addresses: {
+    flex: 1,
+  },
+  font: {
+    fontSize: 12,
+  },
+  headerRow: {
+    marginTop: 4,
+    color: '#444',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
+
 export function MessageComponent(props: {message: Message}): JSX.Element {
   const message = props.message;
+  const headerRowStyles = [style.headerRow, style.font];
   return (
     <>
-      <Text>
-        {'\n'}
-        {message.from && (
-          <Text>
-            {message.getFromAddresses()}
-            {'\n'}
-          </Text>
-        )}
-        {message.to && (
-          <Text>
-            to: {message.getToNames()}
-            {'\n'}
-          </Text>
-        )}
-        {message.cc && (
-          <Text>
-            cc: {message.getCcNames()}
-            {'\n'}
-          </Text>
-        )}
-        {message.formatDate()}
-        {'\n'}
-      </Text>
+      <View style={style.header}>
+        <View style={style.addresses}>
+          {message.from && (
+            <Text style={[style.bold, style.font]} numberOfLines={1}>
+              {message.getFromNames()}
+            </Text>
+          )}
+          {message.to && (
+            <Text style={headerRowStyles} numberOfLines={1}>
+              <Text style={style.bold}>to:</Text> {message.getToNames()}
+            </Text>
+          )}
+
+          {message.cc && (
+            <Text style={headerRowStyles} numberOfLines={1}>
+              <Text style={style.bold}>cc:</Text> {message.getCcNames()}
+            </Text>
+          )}
+        </View>
+        <Text style={headerRowStyles} numberOfLines={1}>
+          {message.formatDate()}
+        </Text>
+      </View>
       <WebView
         scrollEnabled={false}
         source={{html: htmlHeader + message.getHtmlOrPlain()}}
