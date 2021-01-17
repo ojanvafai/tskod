298337caadc6ce69a2433dd6b6030cabd4467872
Ojan Vafai <ojan.vafai@gmail.com>
2021-01-16 19:18:03 -0800

    Speculative fix for re-auth.
    I was able to confirm getting a 401 in cases where the client sat for a while and then I checked for new messages. In theory this should fix it.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -8,23 +8,16 @@ import {
   View,
 } from 'react-native';
 
-import {
-  GoogleSignin,
-  statusCodes,
-  User,
-} from '@react-native-community/google-signin';
-
 import {Card} from './Card';
 import {
   archiveMessages,
   fetchThreads,
   applyLabelToMessages,
-  saveAccessToken,
+  login,
 } from '../Gapi';
 import {Message} from '../Message';
 import {defined} from '../Base';
 import {LabelName, Labels} from '../Labels';
-import {signInRealm} from '../Sync';
 export interface ThreadActions {
   archive: (messages: Message[]) => Promise<Response>;
   keep: (messages: Message[]) => Promise<Response>;
@@ -86,62 +79,7 @@ function App(): JSX.Element {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     (async (): Promise<void> => {
-      GoogleSignin.configure({
-        scopes: [
-          'https://www.googleapis.com/auth/gmail.modify',
-          'https://www.googleapis.com/auth/contacts.readonly',
-        ],
-        webClientId:
-          '957024671877-pmopl7t9j5vtieu207p56slhr7h1pkui.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-        iosClientId:
-          '957024671877-4eu314jmn3c60neao556ltfa025u9ao3.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-        offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-        //hostedDomain: '', // specifies a hosted domain restriction
-        //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
-        //forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
-        //accountName: '', // [Android] specifies an account name on the device that should be used
-      });
-
-      await GoogleSignin.hasPlayServices();
-
-      let user: User | null = null;
-
-      try {
-        user = await GoogleSignin.signInSilently();
-      } catch (error) {
-        if (error.code !== statusCodes.SIGN_IN_REQUIRED) {
-          console.log('Sign in failed', JSON.stringify(error));
-          return;
-        }
-
-        try {
-          user = await GoogleSignin.signIn();
-        } catch (nestedError) {
-          console.log('Sign in failed', JSON.stringify(nestedError));
-          if (nestedError.code === statusCodes.SIGN_IN_CANCELLED) {
-            // user cancelled the login flow
-          } else if (nestedError.code === statusCodes.IN_PROGRESS) {
-            // operation (e.g. sign in) is in progress already
-          } else if (
-            nestedError.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE
-          ) {
-            // play services not available or outdated
-          } else {
-            // some other error happened
-          }
-        }
-      }
-
-      const accessToken = (await GoogleSignin.getTokens()).accessToken;
-      await saveAccessToken(accessToken);
-      console.log('Before realm login');
-      if (user !== null && user.serverAuthCode !== null) {
-        await signInRealm(user.serverAuthCode);
-      } else {
-        console.log('null user or auth code');
-        console.log(JSON.stringify(user));
-      }
-      console.log('After Realm Login');
+      await login();
       await Labels.init();
       setLoadingThreads(LoadState.loading);
     })();
